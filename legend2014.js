04a036296bd64e016abb32e90938cbcb4d5ad740
Nickdemo <d.jurado.serrano@gmail.com>
2016-05-29 14:30:09 +0200

    First commit in SRC

---
+++ legend2014.js
@@ -0,0 +1,42 @@
+var legend2014 = {
+	title: "Leyenda",
+	description: "Caracterización de los usos del suelo en el Poniente Almeriense",
+	sections: [{
+		title: 'Usos del Suelo',
+		className: 'usos',
+		keys: [
+			{
+				coordinates: [36.84419,-2.78606, 15],
+				text: "Arbolado forestal"
+			},
+			{
+				coordinates: [36.791,-3.08321, 15],
+				text: "Frutal"
+			},
+			{
+				coordinates: [36.717838,-2.86271, 15],
+				text: "Forzado"
+			},			
+			{
+				coordinates: [36.82756,-2.69826,15],
+				text: "Huerta"
+			},
+			{
+				coordinates: [36.88244,-3.02673, 15],
+				text: "Masas de agua"
+			},
+			{
+				coordinates: [36.82055,-2.841, 15],
+				text: "Pastizal"
+			},
+			{
+				coordinates: [36.69936,-2.68354, 15],
+				text: "Salinas"
+			},
+			{
+				coordinates: [36.68762,-2.72143, 15],
+				text: "Terrenos con escasa o nula vegetación"
+			}
+		]
+	}
+]};
