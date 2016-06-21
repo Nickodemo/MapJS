c7b79049de8e95ca3bc7ce54dcb851e789109ebf
Nickdemo <d.jurado.serrano@gmail.com>
2016-06-21 19:42:59 +0200

    Prueba primera capa usos suelo. Modificación del nombre de las capas (no pueden empezar por número)

--- Escasa_nula_vegetacion_2014.js
+++ Escasa_nula_vegetacion_2014.js
@@ -1,4 +1,4 @@
-var 2014_Escasa_nula_vegetacion={
+var Escasa_nula_vegetacion_2014={
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                                                                   
