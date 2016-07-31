e09230a882ee69b9d260e42477d82c4c44bfc276
Nickdemo <d.jurado.serrano@gmail.com>
2016-07-31 22:19:45 +0200

    fix elena cabezonabug

--- source/info_municipios.js
+++ source/info_municipios.js
@@ -9,48 +9,48 @@
 		"name":"Adra",
 		"image_name":"Adra.jpg",
 		"hab":24.782,
-		"descrip": "Es el término municipal más occidental de la provincia de Almería. Al oeste limita con la provincia de Granada, al este con el término municipal de Berja y al sur con el Mar Mediterráneo. Su cabecera municipal es la cuarta ciudad más antigua de España y alberga uno de los principales lugares de interés geológico del litoral de Almería, las conocidas Albuferas de Adra; así como la unidad hidrográfica más importante de la comarca: el río Adra."
+		"descrip": "Es el término municipal más occidental de la provincia de Almería. Al oeste limita con la provincia de Granada, al este con el término municipal de Berja y al sur con el Mar Mediterráneo. Su cabecera municipal es la cuarta ciudad más antigua de España y alberga uno de los principales lugares de interés geológico del litoral de Almería, las conocidas Albuferas de Adra, así como la unidad hidrográfica más importante de la comarca: el río Adra."
 	},
 	"dalias":{
 		"name":"Dalías",
 		"image_name":"Dalias.jpg",
-		"hab":3.979;
+		"hab":3.979,
 		"descrip":"El término municipal de Dalías se ubica al abrigo de la Sierra de Gádor, limitando al norte con la Alpujarra almeriense, al oeste con Berja, al sur con El Ejido y al este con Félix y Vícar. Es uno de los municipios que componen la demarcación natural del  Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"elejido":{
 		"name":"El Ejido",
 		"image_name":"El_Ejido.jpg",
-		"hab":84.144;
+		"hab":84.144,
 		"descrip":"El término municipal de El Ejido se enclava entre Dalías al norte, el Mar Mediterráneo al sur, Berja al oeste  y Vícar, La Mojonera y Roquetas de Mar al este. Su principal característica orográfica es que su extensión es en su totalidad la planicie  de 45 m de altura. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"laMojonera":{
 		"name":"La Mojonera",
 		"image_name":"La_ Mojonera.jpg",
-		"hab":8.993;
+		"hab":8.993,
 		"descrip":"El término municipal de La Mojonera limita al norte y este con Vícar, al oeste con El Ejido y al sur con Roquetas de Mar. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"vicar":{
 		"name":"Vícar",
 		"image_name":"Vicar.jpg",
-		"hab":24.384;
+		"hab":24.384,
 		"descrip":"El término municipal limita al norte con Enix y Felix, al sur con La Mojonera y Roquetas de Mar y al oeste está flanqueado por Dalías, El Ejido y La Mojonera. Su principal unidad geográfica es la Sierra de Gádor, como la mayoría de los municipios del Poniente Almeriense. Cuenta con varias ramblas importantes, como la de Carcáuz o la de Cañuelo. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"felix":{
 		"name":"Felix",
 		"image_name":"Félix.jpg",
-		"hab":651;
+		"hab":651,
 		"descrip":"El término municipal de Félix limita al norte con la Alpujarra Almeriense, al sur con Vícar, al oeste con Dalías y al este con Enix. Cuenta con un manantial conocido en toda la provincia por sus excepcional agua. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"enix":{
 		"name":"Enix",
 		"image_name":"Enix.jpg",
-		"hab":440;
+		"hab":440,
 		"descrip":"El término municipal de Énix es el más oriental de la comarca, limitando al norte con la Alpujarra Almeriense, al este con la comarca Metropolitana de Almería, al sur con Roquetas de Mar, y al oeste con Félix y Vícar. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	},
 	"roquetasdemar":{
 		"name":"Roquetas de mar",
 		"image_name":"Roquetas_de_mar.jpg",
-		"hab":91.682;
+		"hab":91.682,
 		"descrip":"El término municipal de Roquetas limita al norte con Énix y Vícar, al oeste con el Ejido y La Mojonera y al sur con el Mar Mediterraneo. Su orografía se caracteriza por ser un terreno en general llano y de escasa altitud. Cuenta con varias ramblas, como la de El Cañuelo,6 la del Pastor y la de la Culebra. Se ubica en la demarcación natural del Campo de Dalías, región conocida por su intensiva agricultura bajo plástico."
 	}
 }
